8e02f9d0cc88fd8efbcc83ab5a7ee10a6fb6713c
Robert Rees <shudderfix@gmail.com>
2018-10-21 15:33:44 +0100

    Creates an initial page

---
+++ hello-next/pages/about.js
@@ -0,0 +1,10 @@
+function About() {
+	return (
+		<section>
+			<h1>About</h1>
+			<p>This is a test app to learn NextJS</p>
+		</section>
+	)
+}
+
+export default About;
---
+++ hello-next/pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => (
+	<div>
+		<p>Hello from NextJS</p>
+	</div>
+)
+
+export default Index
